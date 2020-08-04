975cced7518a76624c5349df1d1cc37706b403b2
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-08-04 12:31:45 -0700

    Updated test run to fetch multiple inteferograms This is necessary to allow mintpy processing to work correctly

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 conda_base=$(conda info --base)
 source "${conda_base}/etc/profile.d/conda.sh"
 conda activate ariaMintpy
-python run_pge.py --bounds "18.8 20.3 -156.1 -154.8" --tracknumber "124" --start "20181215" --end "20190101"
+python run_pge.py --bounds "18.8 20.3 -156.1 -154.8" --tracknumber "124" --start "20181215" --end "20190120"
 #python ../run_pge.py --shapefile "/home/ops/something.file" --tracknumber "124" --start "20181215" --end "20190101"
