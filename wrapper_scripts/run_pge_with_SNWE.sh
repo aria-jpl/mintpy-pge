20eaf355553b34ead808e658203a6ceaf11cadf8
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-08-04 16:51:41 -0700

    Minor path fixes and added mock args to run_pge_with_SNWE.sh

--- wrapper_scripts/run_pge_with_SNWE.sh
+++ wrapper_scripts/run_pge_with_SNWE.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-
 conda_base=$(conda info --base)
 source "${conda_base}/etc/profile.d/conda.sh"
 conda activate ariaMintpy
-python ../run_pge.py
+python /home/ops/verdi/ops/run_pge.py --bounds "18.8 20.3 -156.1 -154.8" --tracknumber "124" --start "20181215" --end "20190101"
