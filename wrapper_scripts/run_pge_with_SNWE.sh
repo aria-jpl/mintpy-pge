89df724072e5a414137f023ac7aec865c7f7d9e6
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-08-05 10:38:55 -0700

    Fixed yet more pathing issues

--- wrapper_scripts/run_pge_with_SNWE.sh
+++ wrapper_scripts/run_pge_with_SNWE.sh
@@ -3,4 +3,4 @@
 conda_base=$(conda info --base)
 source "${conda_base}/etc/profile.d/conda.sh"
 conda activate ariaMintpy
-python /home/ops/verdi/ops/run_pge.py --bounds "18.8 20.3 -156.1 -154.8" --tracknumber "124" --start "20181215" --end "20190101"
+python ${pge_root}/run_pge.py --bounds "18.8 20.3 -156.1 -154.8" --tracknumber "124" --start "20181215" --end "20190101"
