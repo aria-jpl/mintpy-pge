4d861f430c5591c5bd7432d6945bad1149559941
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-08-05 14:11:37 -0700

    Deleted obsolete scripts

--- run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-conda_base=$(conda info --base)
-source "${conda_base}/etc/profile.d/conda.sh"
-conda activate ariaMintpy
-python run_pge.py --bounds "18.8 20.3 -156.1 -154.8" --tracknumber "124" --start "20181215" --end "20190120"
-#python ../run_pge.py --shapefile "/home/ops/something.file" --tracknumber "124" --start "20181215" --end "20190101"
