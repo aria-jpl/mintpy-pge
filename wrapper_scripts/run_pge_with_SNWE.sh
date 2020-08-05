dca0e85704cebd79ba899a2c2b7c018b1f3cf4e9
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-08-05 14:11:25 -0700

    Added explicit sourcing to run_pge_with_SNWE.sh

--- wrapper_scripts/run_pge_with_SNWE.sh
+++ wrapper_scripts/run_pge_with_SNWE.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Ensure conda is available
+source /etc/profile
+source ~/.profile
+source /etc/bashrc
+
+# Activate conda environment
 conda_base=$(conda info --base)
 source "${conda_base}/etc/profile.d/conda.sh"
 conda activate ariaMintpy
+
+# Run PGE
 python ${pge_root}/run_pge.py --bounds "18.8 20.3 -156.1 -154.8" --tracknumber "124" --start "20181215" --end "20190101"
