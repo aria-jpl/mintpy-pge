42e50d7f3ca8f62b3db252c9d303269127186017
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-08-07 11:35:34 -0700

    Some attempted environment fixes per David/Hook's suggestions

--- wrapper_scripts/run_pge_with_SNWE.sh
+++ wrapper_scripts/run_pge_with_SNWE.sh
@@ -2,13 +2,15 @@
 
 # Ensure conda is available
 source /etc/profile
-source ~/.profile
+source /home/ops/.profile
 source /etc/bashrc
+source /home/ops/.bashrc
 
 # Activate conda environment
-conda_base=$(conda info --base)
-source "${conda_base}/etc/profile.d/conda.sh"
-conda activate ariaMintpy
+#conda_base=$(conda info --base)
+#source "${conda_base}/etc/profile.d/conda.sh"
+#conda activate ariaMintpy
+source activate ariaMintpy
 
 # Run PGE
 python ${pge_root}/run_pge.py --bounds "18.8 20.3 -156.1 -154.8" --tracknumber "124" --start "20181215" --end "20190101"
