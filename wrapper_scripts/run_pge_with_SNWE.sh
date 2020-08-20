9a2242e32e0be1d09522b3af639345581aef9524
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-08-20 15:52:18 -0700

    runscript now copies MintPy config across to working directory where MintPy expects it to be

--- wrapper_scripts/run_pge_with_SNWE.sh
+++ wrapper_scripts/run_pge_with_SNWE.sh
@@ -20,5 +20,8 @@ work_dir="${work_root}/$(printf '%(%s)T\n' -1)"
 sudo mkdir ${work_dir} && cd $work_dir || exit 1
 sudo chown ops:ops $work_dir
 
+# Copy MintPy config to working directory
+cp "${pge_root}/smallbaselineApp.cfg" "${work_dir}/smallbaselineApp.cfg"
+
 # Run PGE
 python ${pge_root}/run_pge.py --bounds "18.8 20.3 -156.1 -154.8" --tracknumber "124" --start "20181215" --end "20190121"
