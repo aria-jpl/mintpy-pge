31e30802cb1d3e020095f5df2f955757a6b8a6cd
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-09-03 08:18:18 -0700

    Reworked Dockerfile

--- wrapper_scripts/download_data_products.sh
+++ wrapper_scripts/download_data_products.sh
@@ -7,7 +7,7 @@ start=$4
 end=$5
 download=$6
 
-conda_base=$(conda info --base)
-source "${conda_base}/etc/profile.d/conda.sh"
-conda activate ariaMintpy
+#conda_base=$(conda info --base)
+#source "${conda_base}/etc/profile.d/conda.sh"
+#conda activate ariaMintpy
 ariaDownload.py -t "$track_number" -w "$download_dir" -b "$bounding_box" -s "$start" -e "$end" -o "$download"
