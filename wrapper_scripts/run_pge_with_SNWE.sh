e724a62998cd0a46a575cd07396f1d5fd7e07d97
Alex Dunn <alexander.e.dunn@jpl.nasa.gov>
2020-09-15 13:13:32 -0700

    run_pge_with_SNWE.sh now automatically detects local/mamba env and runs accordingly

--- wrapper_scripts/run_pge_with_SNWE.sh
+++ wrapper_scripts/run_pge_with_SNWE.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "Running PGE wrapper script as $(echo whoami)"
-#echo "/opt/conda/envs/ariaMintpy owned by $(ls -l /opt/conda/envs/ariaMintpy)"
-
 echo "Ensuring conda is available..."
 . /opt/conda/etc/profile.d/conda.sh
 conda activate base
@@ -16,8 +13,6 @@ conda activate ariaMintpy
 
 echo "Checking active conda environment: '${CONDA_DEFAULT_ENV}'"
 echo "Checking PYTHONPATH: ${PYTHONPATH}"
-#echo "Owning pge_root and cwd"
-#sudo chown -R ops:ops $pge_root .
 
 echo "Copying MintPy config to working directory"
 cp "${pge_root}/smallbaselineApp.cfg" "./smallbaselineApp.cfg"
@@ -31,19 +26,20 @@ south_bound=$2
 west_bound=$3
 east_bound=$4
 
-export bounds="${south_bound} ${north_bound} ${west_bound} ${east_bound}"
-export track_number=$5
-export start_date=$6
-export end_date=$7
+bounds="${south_bound} ${north_bound} ${west_bound} ${east_bound}"
+track_number=$5
+start_date=$6
+end_date=$7
 
-export command_to_run='python3 ${pge_root}/run_pge.py --bounds "${bounds}" --tracknumber "${track_number}" --start "${start_date}" --end "${end_date}"'
+command_to_run="python3 ${pge_root}/run_pge.py --bounds "\"$bounds\"" --tracknumber "\"$track_number\"" --start "\"$start_date\"" --end "\"$end_date\"
 echo "About to run: ${command_to_run}"
-
-# Comment out for deployment
-#conda run -n ariaMintpy /bin/bash -c "${command_to_run} > /dev/tty 2>&1"
-
-# Comment out for development
-conda run -n ariaMintpy /bin/bash -c "${command_to_run}"
+if [ $UID = 1003 ]; then
+#  Running on mamba cluster
+  conda run -n ariaMintpy /bin/bash -c "${command_to_run}"
+else
+#  Running in local dev environment
+  conda run -n ariaMintpy /bin/bash -c "${command_to_run} > /dev/tty 2>&1"
+fi
 
 # generate dataset ID
 timestamp=$(date -u +%Y%m%dT%H%M%S.%NZ)
